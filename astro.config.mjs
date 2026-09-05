18c8d539c88793af70891ad4563ecd515359c151
Neel Poluri <mightymickey81@gmail.com>
2026-09-05 01:04:36 -0400

    Project Views done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    fonts: [
+        {
+            provider: fontProviders.fontsource(),
+            name: "Orbitron",
+            cssVariable: "--font-orbitron",    
+            styles: ["normal"],
+            weights: ["300 700"],
+        },
+        {
+            provider: fontProviders.fontsource(),
+            name: "Cascadia Code",
+            cssVariable: "--font-cascadia-code",
+            styles: ["normal"],
+            weights: ["400 700"],
+        }
+    ],
+});
