55ef0216b3e26fb9acb675e410ddc006720435ec
Neel Poluri <mightymickey81@gmail.com>
2026-09-05 23:18:46 -0400

    update site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://renovic.github.io',
-    base: '/website',
+    site: 'https://neels.page',
     fonts: [
         {
             provider: fontProviders.fontsource(),
